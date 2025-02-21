4981e91e0f35959f54ddf13168d33dcc82d754d2
bigchills <yemi7164@gmail.com>
2025-02-21 09:43:46 +0100

    feat: enhance responsivenessfor about page hero section

--- app/components/about-hero-section.tsx
+++ app/components/about-hero-section.tsx
@@ -3,20 +3,30 @@ import Star from "../svg/Star";
 
 function AboutHeroSection() {
   return (
-    <div className="text-center pt-[180px] flex flex-col items-center px-[100px]">
-      <h4 className="font-AgrandirRegular text-ash text-base mb-2">
-        About TokenGiver
-      </h4>
-      <h1 className="font-AgrandirBold text-5xl leading-[70px] max-w-[668px] mb-2 text-raisin-black">
-        Where Giving Meets <span className="text-primary">Innovation</span>
-      </h1>
-      <div className="py-[15px] px-3 flex items-center gap-x-[10px] text-sm text-black shadow-primary-black rounded-[10px] w-fit mb-6">
-        <span className="text-ash">
-          <Star />
-        </span>
-        What exactly do we do?
+
+    
+    <div className="text-center pt-[180px] flex flex-col items-center  md:px-10 lg:px-20 xl:px-[100px] 2xl:max-w-[1440px] 2xl:mx-auto">
+
+      <div className="px-6">
+        <h4 className="font-AgrandirRegular  text-ash text-base mb-2">
+          About TokenGiver
+        </h4>
+        <div>
+
+        </div>
+        <h1 className="font-AgrandirBold text-[40px]  leading-[52px] sm:leading-[60px] sm:text-5xl xl:leading-[70px] max-w-[668px] mb-2 text-raisin-black">
+          Where Giving Meets <span className="text-primary">Innovation</span>
+        </h1>
+        <div className="py-[15px] px-3 flex justify-center mx-auto items-center gap-x-[10px] text-sm text-black shadow-primary-black rounded-[12px] w-fit mb-6">
+          <span className="text-ash">
+            <Star />
+          </span>
+          What exactly do we do?
+        </div>
       </div>
-      <div className="rounded-[10px] p-[15px] flex gap-x-[10px] items-start text-left shadow-primary-black z-[3] relative bg-white">
+
+
+      <div className="rounded-[10px] p-[16px] hidden  md:flex gap-x-[12px]   items-start text-left shadow-primary-black z-[3] relative bg-white">
         <div className="p-1 bg-primary rounded-[7px]">
           <img src="/logo-white.svg" alt="" />
         </div>
@@ -30,24 +40,30 @@ function AboutHeroSection() {
           </p>
         </div>
       </div>
+
       <div className="w-full relative mt-[-12px]">
         <img
           src="/about-hero-banner.jpg"
-          className="w-full rounded-[10px]"
+          className="w-full rounded-[10px] hidden md:block"
           alt=""
         />
-        <div className="px-[57px] pt-8">
-          <p className="text-base text-ash leading-[35px] mb-7">
+        <img
+          src="/heromobile.png"
+          className="w-full  md:hidden"
+          alt=""
+        />
+        <div className="px-6 md:px-[57px] pt-8">
+          <p className="text-[18px] leading-9 md:text-base text-ash md:leading-[35px] mb-7">
             At TokenGiver, our mission is to revolutionize the world of
             fundraising by merging the best of blockchain technology with the
             spirit of giving. We aim to create a world where every donation is
             not only impactful but also secure, transparent, and accountable.
           </p>
-          <div className="flex justify-center items-center gap-x-3">
-            <button className="py-4 px-[47px] rounded-full bg-primary font-semibold text-sm leading-[23px] text-white">
+          <div className="flex flex-col sm:flex-row gap-4  justify-center items-center md:gap-x-3">
+            <button className="py-5 px-[47px] md:py-4 rounded-full w-full md:w-[250px] bg-primary font-semibold text-sm leading-[23px] text-white">
               Start a Campaign
             </button>
-            <button className="bg-white py-4 rounded-full w-[250px] text-primary border-primary border font-semibold text-sm">
+            <button className="bg-white py-5 md:py-4 rounded-full w-full md:w-[250px] text-primary border-primary border font-semibold text-sm">
               Explore Categories
             </button>
           </div>
