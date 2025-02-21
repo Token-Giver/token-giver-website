18bcdf700ef6f768caa7694c56edcec09025657b
bigchills <yemi7164@gmail.com>
2025-02-21 09:44:56 +0100

    feat:enhance responsiveness for who we are section

--- app/components/who-we-are.tsx
+++ app/components/who-we-are.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function WhoWeAre() {
   return (
-    <div className="px-[100px] py-[100px]">
-      <div className="flex justify-between items-center">
-        <div className="max-w-[600px]">
-          <h2 className="mb-[30px] font-AgrandirBold text-3xl">
+    <div className="px-6 md:px-10 lg:px-20 xl:px-[100px] py-[100px] space-y-10 sm:space-y-12 lg:space-y-20 2xl:max-w-[1440px] 2xl:mx-auto">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-5">
+        <div className=" py-4 space-y-7 lg:flex-1">
+          <h2 className=" font-AgrandirBold text-3xl">
             Who <span className="font-AgrandirRegular">We</span> Are?
           </h2>
           <p className="text-base leading-8 text-ash font-medium">
@@ -17,14 +17,16 @@ function WhoWeAre() {
             meaningful change backed by cutting-edge technology.
           </p>
         </div>
-        <img src="/who-we-are-illus.png" alt="" />
+        <div className="w-full lg:flex-1 lg:flex lg:justify-end">
+          <img className="w-full lg:w-5/6 " src="/who-we-are-illus.png" alt="" />
+        </div>
       </div>
-      <div className="flex justify-between mt-[100px]">
-        <h2 className="mb-[30px] font-AgrandirBold text-3xl max-w-[469px]">
+      <div className="flex flex-wrap space-y-4 md:space-y-7 justify-between  items-center lg:pt-10">
+        <h2 className=" font-AgrandirBold text-3xl leading-[36px] sm:leading-[48px] xl:text-[40px] xl:leading-[60px]  sm:w-5/6  lg:w-2/3 xl:w-1/2">
           Be part of a <span className="font-AgrandirRegular">community</span>{" "}
           dedicated to making a difference.
         </h2>
-        <p className="text-base leading-8 text-ash font-medium text-left max-w-[624px]">
+        <p className="text-base leading-8 text-ash font-medium  w-full text-left lg:w-5/6 xl:w-1/2">
           With TokenGiver, your donations are more than just
           contributions—they're investments in a better tomorrow.
         </p>
