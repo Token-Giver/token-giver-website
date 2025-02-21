47786707856f9a9ff4e71f60aab9648bcfc6aa87
Billy <tobiikupolati33@gmail.com>
2025-02-21 03:19:08 +0100

    feat: improve EasySteps component responsiveness and styling

--- app/components/ui/step-block.tsx
+++ app/components/ui/step-block.tsx
@@ -1,34 +1,49 @@
-import { Step } from "@/app/utils/types";
-import React from "react";
+import { Step } from '@/app/utils/types';
+import React from 'react';
 
 function StepBlock({ step }: { step: Step }) {
   return (
     <div
       className={`flex ${
-        step.id % 2 ? "flex-row-reverse" : "flex-row"
-      } items-center justify-between pt-5`}
+        step.id % 2 ? 'md:flex-row-reverse' : 'md:flex-row'
+      } md:items-center justify-center md:justify-between pt-2 md:pt-5 gap-4 md:gap-10 flex-col`}
     >
-      <div className="relative font-AgrandirBold text-black text-lg leading-8">
-        <img src={step.illus} className="w-[547px] h-[555px]" alt="" />
+      {/* Step Number on the image for small screen */}
+      <div className={` md:hidden flex gap-x-[10px]  items-center`}>
+        <div className='h-10 w-10 flex items-center justify-center bg-white border-[.4px] border-[#D9D9D9] rounded-full text-center'>
+          {step.id + 1}
+        </div>
+        <div>Step</div>
+      </div>
+
+      <div className='relative max-md:order-3 font-AgrandirBold text-black text-lg leading-8 flex-1 flex justify-center items-center  max-w-[547px] max-h-[555px]'>
+        <img
+          src={step.illus}
+          className='w-full h-full object-cover'
+          alt={step.subtext + ' illustration'}
+        />
+
+        {/* Step Number on the image for large screen */}
         <div
-          className={`flex gap-x-[10px] absolute -top-[15px] items-center ${
+          className={`hidden min-[850px]:flex gap-x-[10px] absolute -top-[15px] items-center ${
             step.id % 2
-              ? "-left-[71px] flex-row-reverse"
-              : "-right-[71px] flex-row"
+              ? '-left-[71px] flex-row-reverse'
+              : '-right-[71px] flex-row'
           }`}
         >
-          <div className="py-[7px] px-4 bg-white border-[.4px] border-[#D9D9D9] rounded-full text-center">
+          <div className='h-10 w-10 flex items-center justify-center bg-white border-[.4px] border-[#D9D9D9] rounded-full text-center'>
             {step.id + 1}
           </div>
           <div>Step</div>
         </div>
       </div>
-      <div className="text-ash font-medium max-w-[600px]">
-        <h2 className="text-xl font-AgrandirBold mb-4 text-raisin-black">
+
+      <div className='text-ash font-medium max-w-[600px] flex-1 max-md:order-2'>
+        <h2 className='text-2xl font-AgrandirBold mb-3 text-raisin-black'>
           {step.title}
         </h2>
         <p>{step.subtext}</p>
-        <ul className="list-disc ml-[30px] text-base leading-8">
+        <ul className='list-disc ml-[30px] text-base leading-8'>
           {step.points.map((point, i) => (
             <li key={i}>{point}</li>
           ))}
