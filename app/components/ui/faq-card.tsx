df01b2a1c4ff0b903f1a12ba6835ad34d4097307
Billy <tobiikupolati33@gmail.com>
2025-02-21 03:28:14 +0100

    feat: improve FAQ section responsiveness and styling

--- app/components/ui/faq-card.tsx
+++ app/components/ui/faq-card.tsx
@@ -1,4 +1,4 @@
-import DownChevronIcon from "@/app/svg/DownChevronIcon";
+import DownChevronIcon from '@/app/svg/DownChevronIcon';
 
 function FaqCard({
   faq,
@@ -10,15 +10,15 @@ function FaqCard({
   toggleAccordion: () => void;
 }) {
   return (
-    <div className="border-b-solid cursor-pointer border border-x-transparent border-b-foreground-secondary border-t-transparent">
+    <div className='border-b-solid cursor-pointer border border-x-transparent border-b-foreground-secondary border-t-transparent'>
       <button
-        className="flex w-full items-center justify-between p-4 text-left focus:outline-none"
+        className='flex w-full items-center justify-between max-[430px]:px-1 p-4 text-left focus:outline-none'
         onClick={toggleAccordion}
       >
-        <span className="font-medium">{faq.question}</span>
+        <span className='font-medium'>{faq.question}</span>
         <span
           className={`transform transition-transform duration-200 ${
-            isActive ? "rotate-180" : ""
+            isActive ? 'rotate-180' : ''
           }`}
         >
           <DownChevronIcon />
@@ -26,11 +26,11 @@ function FaqCard({
       </button>
       <div
         className={`grid transition-all duration-200 ease-in-out ${
-          isActive ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
+          isActive ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'
         }`}
       >
-        <div className="overflow-hidden">
-          <div className="p-4 pt-0 text-raisin-black">{faq.answer}</div>
+        <div className='overflow-hidden'>
+          <div className='p-4 pt-0 text-raisin-black'>{faq.answer}</div>
         </div>
       </div>
     </div>
