aa1056c35e0cdcc08d980013394fc3af8df744a2
bigchills <yemi7164@gmail.com>
2025-02-21 09:51:52 +0100

    feat:enhance responsiveness for campaign section

--- app/components/start-campaign-banner.tsx
+++ app/components/start-campaign-banner.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function StartCampaignBanner() {
   return (
-    <div className="px-[100px] py-[60px] text-center">
-      <div className="bg-[#FEB3D2] py-[75px] w-full flex flex-col items-center rounded-[10px]">
+    <div className=" sm:px-10 lg:px-20 xl:px-[100px] py-[60px] text-center 2xl:max-w-[1440px] 2xl:mx-auto">
+      <div className="bg-[#FEB3D2] px-6 py-24 w-full flex flex-col items-center md:rounded-[12px]">
         <h3 className="mb-4 font-AgrandirRegular text-3xl">
           <span className="font-AgrandirBold">Start a</span> Campaign
         </h3>
-        <p className="mb-10 text-lg max-w-[638px]">
+        <p className="mb-10 text-xl max-w-[638px]">
           <span className="font-semibold">Looking to make a difference?</span>{" "}
           Launch your fundraising campaign on{" "}
           <span className="font-semibold">TokenGiver</span> and tap into the
